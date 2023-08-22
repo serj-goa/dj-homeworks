6f475dcc376e7c08a9305feec8207bb073b9413b
Serhii Borodin <bsy.prog@gmail.com>
2023-08-22 12:58:54 +0300

    Separated permissions

--- 3.3-permissions/api_with_restrictions/advertisements/permissions.py
+++ 3.3-permissions/api_with_restrictions/advertisements/permissions.py
@@ -1,9 +1,12 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsOwnerOrReadOnly(BasePermission):
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.creator
+
+
+class ReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method == 'GET':
             return True
-
-        return request.user == obj.creator
