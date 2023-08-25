30f15911765c3a763147dba53638af31fa3bd99a
Serhii Borodin <bsy.prog@gmail.com>
2023-08-25 12:29:41 +0300

    Add new max students at course test

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -30,7 +30,7 @@ def students_factory():
 @pytest.mark.django_db
 def test_get_first_course(client, course_factory):
     course = course_factory(_quantity=1)
-    response = client.get('/api/v1/courses/1/')
+    response = client.get(f'/api/v1/courses/{course[0].id}/')
     response_data = response.json()
 
     assert response.status_code == 200
@@ -53,7 +53,7 @@ def test_get_all_courses(client, course_factory):
 @pytest.mark.django_db
 def test_filter_course_by_id(client, course_factory):
     courses = course_factory(_quantity=10)
-    response = client.get('/api/v1/courses/', {'id': courses[0].id})
+    response = client.get('/api/v1/courses/', data={'id': courses[0].id})
     response_data = response.json()
 
     assert response.status_code == 200
@@ -63,7 +63,7 @@ def test_filter_course_by_id(client, course_factory):
 @pytest.mark.django_db
 def test_filter_course_name(client, course_factory):
     courses = course_factory(_quantity=10)
-    response = client.get(f'/api/v1/courses/?name={courses[0].name}')
+    response = client.get('/api/v1/courses/', data={'name': courses[0].name})
     response_data = response.json()
 
     assert response.status_code == 200
@@ -98,13 +98,23 @@ def test_delete_course(client, course_factory):
     assert Course.objects.count() == count - 1
 
 
+# @pytest.mark.django_db
+# def test_max_students_at_course(client, students_factory):
+#     students = students_factory(_quantity=21)
+#
+#     course = Course.objects.create(name='test_course_name')
+#     course.save()
+#
+#     for student in students:
+#         course.students.add(student)
+#         course.save()
+
+
+@pytest.mark.parametrize('students_count,status_code', [(19, 201), (20, 201), (21, 400), ])
 @pytest.mark.django_db
-def test_max_students_at_course(client, students_factory):
-    students = students_factory(_quantity=21)
+def test_max_students_at_course(client, students_factory, students_count, status_code):
+    students = students_factory(_quantity=students_count)
+    students_ids = [student.id for student in students]
+    response = client.post('/api/v1/courses/', data={'name': 'Test', 'students': students_ids})
 
-    course = Course.objects.create(name='test_course_name')
-    course.save()
-
-    for student in students:
-        course.students.add(student)
-        course.save()
+    assert response.status_code == status_code
