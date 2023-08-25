d4c1c8cc5bb1377fb3769635697bf1dadfbebdbf
Serhii Borodin <bsy.prog@gmail.com>
2023-08-25 12:28:39 +0300

    Add students validate

--- 3.4-django-testing/django_testing/students/serializers.py
+++ 3.4-django-testing/django_testing/students/serializers.py
@@ -1,10 +1,17 @@
-from rest_framework import serializers
+from django.conf import settings
+from rest_framework import serializers as s
 
 from students.models import Course
 
 
-class CourseSerializer(serializers.ModelSerializer):
+class CourseSerializer(s.ModelSerializer):
 
     class Meta:
         model = Course
         fields = ("id", "name", "students")
+
+    def validate_students(self, data):
+        if len(data) > settings.MAX_STUDENTS_PER_COURSE:
+            raise ValueError(f'Превышено количество студентов!\nМаксимум = {settings.MAX_STUDENTS_PER_COURSE}')
+
+        return data
